bb509c5280596af3ba814fc144200026748822db
milanrolie <112857190+milanrolie@users.noreply.github.com>
2023-07-10 14:43:26 +0200

    coding in the hospital

---
+++ src/lib/OtherProjects.svelte
@@ -0,0 +1,81 @@
+<script>
+</script>
+
+<section>
+    <h3>Other projects</h3>
+  <ul>
+    <li><p>Project over iets</p> <p class="date2">10-11-2019</p></li>
+    <li><p>50k for a better planet</p> <p class="date2">10-11-2019</p></li>
+    <li><p>Commentary tool</p> <p class="date2">10-11-2019</p></li>
+    <li><p>Project over iets</p> <p class="date2">10-11-2019</p></li>
+    <li><p>over iets Anders</p> <p class="date2">10-11-2019</p></li>
+    <li><p>Ink Rio de Janeiro</p> <p class="date2">10-11-2019</p></li>
+  </ul>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    position: relative;
+    width: 100%;
+    /* background-color: rgb(193, 193, 255); */
+    padding: 0;
+    padding-top: 5%;
+  }
+
+  h3 {
+    position: absolute;
+    top: 0;
+    right: 0;
+    color: var(--dark);
+    font-size: 6vw;
+    text-transform: none;
+    line-height: 0.8em;
+    font-weight: 700;
+    text-transform: uppercase;
+
+  }
+
+  ul {
+    /* width: 70%; */
+  }
+
+  li {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    text-decoration: none;
+    list-style: none;
+    font-size: 4vw;
+    border-top: solid 2px;
+    padding: 1px;
+    padding: 5px 2%;
+    transition: .3s;
+
+  }
+
+  p {
+    color: var(--dark);
+    transition: .3s;
+  }
+
+  li:hover p {
+     color: var(--bitterSweet);
+    }
+    
+    li:hover {
+        background-color: rgb(202, 202, 202);
+        cursor: pointer;
+  }
+
+  li:last-child {
+    border-bottom: solid 2px;
+  }
+
+  ul .date2 {
+    font-size: .5em;
+  }
+</style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,214 @@
 <script>
-    import Nav from "$lib/Nav.svelte";
+  import Nav from "$lib/Nav.svelte";
+  import OtherProjects from "$lib/OtherProjects.svelte";
+  import Footer from "$lib/Footer.svelte";
+  import { gsap } from "gsap";
+  import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+  import { onMount } from "svelte";
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  onMount(() => {
+    gsap.to(".words", {
+      scrollTrigger: {
+        trigger: ".hero-wrapper",
+        scrub: 1,
+        start: "top+=20",
+        end: "center+=200",
+      },
+      y: -150,
+    });
+
+    gsap.from(".words", {
+      y: 900,
+      delay: 1,
+      duration: 1.5,
+      ease: "power4.out",
+    });
+
+    gsap.to(".first-project-image", {
+      scrollTrigger: {
+        trigger: ".image",
+        scrub: 1,
+        start: "top-=200",
+        end: "center+=200",
+      },
+      scale: 1.07,
+    });
+
+    gsap.from(".first-project-image", {
+      y: 500,
+      delay: 1.3,
+      duration: 1.5,
+      ease: "power4.out",
+    });
+
+    gsap.from(".date", {
+      y: 500,
+      delay: 2,
+      duration: 1.5,
+      ease: "power4.out",
+    });
+  });
 </script>
 
-  <Nav />
+<Nav />
 
-  <style>
-  
-  </style>
-  
+<section class="project-intro">
+  <div class="text-wrapper">
+    <h1 class="words">Ultimate frisbee commentary tool</h1>
+  </div>
+  <div class="image">
+    <p class="date">23-10-2020</p>
+    <img
+      class="first-project-image"
+      src="https://images.unsplash.com/photo-1611179892587-26e9518c90d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1045&q=80"
+      alt=" "
+    />
+  </div>
+  <div class="project-description">
+    <div class="introduction">
+      <p>
+        Flowers are nature's delicate gems, adorning the world with their
+        vibrant hues and captivating fragrances. They possess a unique ability
+        to convey emotions, symbolizing love, gratitude, or sorrow. From the
+        elegant roses to the cheerful sunflowers, each blossom tells a story,
+        captivating our senses and uplifting our spirits.
+      </p>
+      <p>
+        Beyond their aesthetic appeal, flowers play an essential role in
+        ecosystems, attracting pollinators and supporting biodiversity. They
+        bring life to gardens, parks, and landscapes, creating breathtaking
+        displays of color.
+      </p>
+    </div>
+    <div class="images">
+      <img
+        class="second-project-image"
+        src="https://images.unsplash.com/photo-1623658962582-a09214e103e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1035&q=80"
+        alt=""
+      />
+      <img
+        class="second-project-image"
+        src="https://www.mockupworld.co/wp-content/uploads/dynamic/2022/12/ipad-pro-m2-free-mockup-1072x0-c-default.jpg"
+        alt=""
+      />
+    </div>
+    <div class="introduction">
+      <p>
+        Flowers are nature's delicate gems, adorning the world with their
+        vibrant hues and captivating fragrances. They possess a unique ability
+        to convey emotions, symbolizing love, gratitude, or sorrow. From the
+        elegant roses to the cheerful sunflowers, each blossom tells a story,
+        captivating our senses and uplifting our spirits.
+      </p>
+      <p>
+        Beyond their aesthetic appeal, flowers play an essential role in
+        ecosystems, attracting pollinators and supporting biodiversity. They
+        bring life to gardens, parks, and landscapes, creating breathtaking
+        displays of color.
+      </p>
+    </div>
+    <div class="images">
+      <img
+        class="third-project-image"
+        src="https://images.unsplash.com/photo-1574281583557-6cd118037a4b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
+        alt=""
+      />
+    </div>
+  </div>
+</section>
+
+
+<OtherProjects />
+<Footer />
+
+<style>
+  section {
+    padding: 0;
+  }
+
+  .text-wrapper {
+    position: sticky;
+    top: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    height: 60vh;
+    padding: 2%;
+    margin-bottom: 2%;
+    z-index: -1;
+  }
+
+  h1 {
+    color: var(--dark);
+    font-size: 9vw;
+    text-transform: none;
+    line-height: 0.8em;
+  }
+
+  .date {
+    position: absolute;
+    right: 1%;
+    top: -6%;
+    font-weight: 400;
+    padding: 0.5em 1em;
+    border: solid 2px;
+    border-radius: 1em;
+    color: var(--gunMetal);
+    background-color: var(--dark);
+  }
+
+  .image {
+    position: relative;
+    margin-bottom: -5px;
+  }
+
+  img {
+    object-fit: cover;
+    z-index: 2;
+  }
+
+  .first-project-image {
+    width: 100%;
+    height: 95vh;
+  }
+
+  .project-description {
+    background-color: var(--gunMetal);
+    padding-top: 7%;
+  }
+
+  .introduction {
+    display: flex;
+    flex-direction: row;
+    gap: 5%;
+    padding: 3% 20%;
+    margin-bottom: 3%;
+  }
+
+  .introduction p {
+    font-size: 1.2em;
+    font-weight: 300;
+    max-width: 40%;
+  }
+
+  .images {
+    display: flex;
+    flex-direction: row;
+    gap: 2%;
+    padding: 0% 20%;
+    width: 100%;
+  }
+
+  .second-project-image {
+    width: 50%;
+    height: 75vh;
+  }
+
+  .third-project-image {
+    width: 100%;
+    height: 75vh;
+    margin-bottom: 10%;
+  }
+</style>
